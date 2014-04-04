d8dc7c4b0dd1d822439143dddc2d17a9c0db0627
Professional Mole <professional.mole@yahoo.com.br>
2014-04-04 18:06:08 -0300

    Release script improved
    Added platform specific flags and unzipped release(for easier Windows
    testing)

--- love-release.sh
+++ love-release.sh
@@ -3,14 +3,67 @@
 
 #set -x
 
+buildall=true
+buildmac=false
+buildlinux=false
+buildwin32=false
+buildwin64=false
+ziprelease=true
+keepunzipped=false
+
 if [ $# -lt 1 ]
 then
-	echo "USAGE: love-release.sh <LOVE executable>"
+	echo "USAGE: love-release.sh [(--mac|--linux|--win32|--win64|--nozip|--keep)...] <LOVE executable>"
 	exit 0
 fi
 
-lovefile=$1
-releasedir=$(dirname $1)/release
+while [ $1 ]
+	do
+	echo $1
+	case $1 in
+	--usage)
+		echo "USAGE: love-release.sh [(--mac|--linux|--win32|--win64|--allwin|--nozip|--keep)...] <LOVE executable>"
+		exit 0
+		;;
+	--mac)
+		buildall=false
+		buildmac=true
+		;;
+	--linux)
+		buildall=false
+		buildlinux=true
+		;;
+	--win32)
+		buildall=false
+		buildwin32=true
+		;;
+	--win64)
+		buildall=false
+		buildwin64=true
+		;;
+	--allwin)
+		buildall=false
+		buildwin32=true
+		buildwin64=true
+		;;
+	--mac)
+		buildall=false
+		buildmac=true
+		;;
+	--nozip)
+		ziprelease=false
+		;;
+	--keep)
+		keepunzipped=true
+		;;
+	*)
+		lovefile=$1
+		;;
+	esac
+	shift
+done
+
+releasedir=$(dirname $lovefile)/release
 
 if [ ! -e $lovefile ]
 then
@@ -25,56 +78,100 @@ zipname=$(basename ${lovefile%.love})
 pwd 
 
 function linux {
-	mkdir tmp
-	cp $1 tmp/
-	cd tmp
-	zip -9 -m -q ../$releasedir/$zipname-linux.zip *.love
-	cd ..
-	rmdir tmp
-	echo "DONE!"
+	if $buildall || $buildlinux
+	then
+		mkdir tmp
+		cp $lovefile tmp/
+		cd tmp
+		if $ziprelease
+		then
+			zip -9 -m -q ../$releasedir/$zipname-linux.zip *.love
+		else
+			mv *.love ../$releasedir/
+		fi
+		cd ..
+		rmdir tmp
+		echo "DONE!"
+	else
+		echo "Nothing done"
+	fi
 }
 
 function macosx {
-	mkdir tmp
-	cp $1 tmp/
-	cd tmp
-	unzip -q ../release/macosx/love-macosx.zip
-	appname=$zipname.app
-	mv love.app $appname
-	mv $(basename $1) $appname/Contents/Resources/
-	zip -9 -r -m -q ../$releasedir/$zipname-macosx.zip $appname
-	rm -rf $appname
-	cd ..
-	rmdir tmp
-	echo "DONE!"
+	if $buildall || $buildmac
+	then
+		mkdir tmp
+		cp $lovefile tmp/
+		cd tmp
+		unzip -q ../release/macosx/love-macosx.zip
+		appname=$zipname.app
+		mv love.app $appname
+		mv $(basename $lovefile) $appname/Contents/Resources/
+		if $ziprelease
+		then
+			zip -9 -r -m -q ../$releasedir/$zipname-macosx.zip $appname
+		else
+			mv $appname ../$releasedir/
+		fi
+		rm -rf $appname
+		cd ..
+		rmdir tmp
+		echo "DONE!"
+	else
+		echo "Nothing done"
+	fi
 }
 
 function win32 {
-	mkdir tmp
-	cp $1 tmp/
-	cd tmp
-	unzip -j -q ../release/win32/love-win32.zip
-	appname=$zipname-win32.exe
-	cat love.exe $(basename $1) > $appname
-	rm love.exe *.love
-	zip -9 -m -q ../$releasedir/$zipname-win32.zip *
-	cd ..
-	rmdir tmp
-	echo "DONE!"
+	if $buildall || $buildwin32
+	then
+		mkdir tmp
+		cp $lovefile tmp/
+		cd tmp
+		unzip -j -q ../release/win32/love-win32.zip
+		appname=$zipname-win32.exe
+		cat love.exe $(basename $lovefile) > $appname
+		rm love.exe *.love
+		chmod a+x *
+		if $ziprelease
+		then
+			zip -9 -m -q ../$releasedir/$zipname-win32.zip *
+		else
+			mkdir -p ../$releasedir/$zipname-win32
+			mv * ../$releasedir/$zipname-win32
+		fi
+		cd ..
+		rmdir tmp
+		echo "DONE!"
+	else
+		echo "Nothing done"
+	fi
 }
 
 function win64 {
-	mkdir tmp
-	cp $1 tmp/
-	cd tmp
-	unzip -j -q ../release/win64/love-win64.zip
-	appname=$zipname-win64.exe
-	cat love.exe $(basename $1) > $appname
-	rm love.exe *.love
-	zip -9 -m -q ../$releasedir/$zipname-win64.zip *
-	cd ..
-	rmdir tmp
-	echo "DONE!"
+	if $buildall || $buildwin64
+		then
+		mkdir tmp
+		cp $lovefile tmp/
+		cd tmp
+		unzip -j -q ../release/win64/love-win64.zip
+		appname=$zipname-win64.exe
+		cat love.exe $(basename $lovefile) > $appname
+		rm love.exe *.love
+		chmod a+x *
+		if $ziprelease
+		then
+			zip -9 -m -q ../$releasedir/$zipname-win64.zip *
+		else
+			mkdir -p ../$releasedir/$zipname-win64
+			mv * ../$releasedir/$zipname-win64
+		fi
+		cd ..
+		rmdir tmp
+		echo "DONE!"
+	else
+		echo "Nothing done"
+	fi
 }
 
 for platform in release/*
