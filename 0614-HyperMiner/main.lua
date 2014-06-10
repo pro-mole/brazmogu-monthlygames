a3fc2e89b26e42286494cb5eee0dd93cdb79e637
Professional Mole <professional.mole@yahoo.com.br>
2014-06-10 18:38:09 -0300

    MINOR: Universal constant out of the function Doing some visualization tests

--- 0614-HyperMiner/main.lua
+++ 0614-HyperMiner/main.lua
@@ -24,9 +24,11 @@ function print_debug(...)
 end
  
 function love.load()
-	Probe.new({name = "Tiny", x = 400, y = 172, v = 32, dir = 0, mass = 0.01, size = 8, active = true})
+	Probe.new({name = "Tiny", x = 400, y = 44, v = 4, dir = 0, mass = 0.01, size = 8, active = true})
+	-- Space Station Test
+	Body.new({name = "Station", x = 400, y = 300, v = 0, dir = 0, mass = 64, size = 16, class = 1})
 	--Star.new({name = "Huge", x = 400, y = 300, v = 0, dir = 0, mass = 128, size = 64})
-	Planet.new({name = "Big1", x = 400, y = 300, v = 0, dir = 0, mass = 16, size = 32})
+	--Planet.new({name = "Big1", x = 400, y = 300, v = 0, dir = 0, mass = 16, size = 32})
 	--Planet.new({name = "Big2", x = 656, y = 300, v = 64, dir = math.rad(270), mass = 16, size = 32, atmosphere = {"oxygen", "hydrogen", "water"}, atmosphere_size = 64})
 end
 
