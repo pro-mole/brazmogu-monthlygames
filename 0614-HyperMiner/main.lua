cf96f97d6e36df3850a12fd4389e4d4f7c16dee7
Bruno Guedes <professional.mole@yahoo.com.br>
2014-06-16 01:25:34 -0300

    Tiled Background and very basic HUD

--- 0614-HyperMiner/main.lua
+++ 0614-HyperMiner/main.lua
@@ -2,6 +2,8 @@ require("physics")
 require("draw")
 require("texture")
 
+font = {standard = love.graphics.newFont("assets/font/imagine_font.otf", 10)}
+
 Space = {
 	probes = {},
 	stars = {},
@@ -14,6 +16,8 @@ Space = {
 
 Radar = love.graphics.newCanvas(128,128)
 NavWheel = love.graphics.newCanvas(128,128)
+
+SpaceBG = generateBackground(1024, 0.64)
 
 radar_color = {
 	{255,255,255,255}, -- Class 0
@@ -39,23 +43,19 @@ function print_debug(...)
 end
  
 function love.load()
-	Probe.new({name = "Tiny", x = 400, y = 800, v = 0, dir = 0, mass = 1, size = 8, active = true})
+	Probe.new({name = "Probe", x = 0, y = 1024, v = 8.5, dir = math.pi*1.5, mass = 1, size = 8, active = true})
 	
-	-- Space Station Test
-	Body.new({name = "Station", x = 400, y = 1196, v = 8, dir = 0, mass = 16, size = 16, class = 1,
-		texture = { {"gradient", {255,255,255,255}, {128,128,128,255}} } })
-	
-	--Star.new({name = "Huge", x = 400, y = 300, v = 0, dir = 0, mass = 128, size = 64})
-	--Planet.new({name = "Big1", x = 400, y = 300, v = 0, dir = 0, mass = 16, size = 32})
-	--Planet.new({name = "Big2", x = 656, y = 300, v = 64, dir = math.rad(270), mass = 16, size = 32, atmosphere = {"oxygen", "hydrogen", "water"}, atmosphere_size = 64})
-	
-	-- Planetary System Test
-	Planet.new({name = "Terra", x = 400, y = 300, v = 0, dir = 0, vrot = math.pi/16, mass = 1024, size = 64, atmosphere = {"oxygen", "hydrogen", "water"}, atmosphere_size = 128,
-		texture = { {"gradient", {0, 192, 255, 255}, {0, 96, 128, 255}}, {"scatter", {192, 144, 0, 204}, 0.8}} })
-	Body.new({name = "Luna", x = 400, y = 44, v = 32/math.sqrt(2), dir = 0, vrot = -math.pi/32, mass = 128, size = 16, class = 2,
-		texture = { {"solid", {192, 0, 0, 255}}, {"scatter", {0, 0, 0, 192}, 0.5}} })
-	Body.new({name = "Selene", x = 400, y = 1324, v = 16, dir = 0, vrot = -math.pi/4, mass = 64, size = 24, class = 2,
-		texture = { {"solid", {192, 144, 255, 255}}, {"blotch", {128, 102, 48, 64}, 3, 0.7}, {"blotch", {72, 64, 16, 64}, 7, 0.5}} })
+	-- Test environment
+	-- A planetary system with two moons around it
+	-- Also some asteroids and a comet around
+	Planet.new({name = "Terra", x = 1024, y = 1024, v = 0, dir = 0, vrot = math.pi/8, mass = 128, size = 256, atmosphere = {H=0.1, O=0.6, H20=0.3}, atmosphere_size = 320,
+		texture = { {"gradient", {64, 128, 144, 255}, {12, 64, 96, 255}, 50}, {"scatter", {0, 0, 128, 128}, 0.8}, {"blotch", {0, 64, 0, 204}, 8, 0.8} } })
+	Body.new({class = 2, name = "Luna", x = 1024, y = 1024 + 640, v = 14, dir = 0, vrot = -math.pi/16, mass = 24, size = 16,
+		texture = { {"gradient", {128, 32, 32, 255}, {72, 0, 0, 255}, 32}, {"scatter", {0, 0, 0, 128}, 0.5} } })
+	Body.new({class = 2, name = "Selena", x = 64, y = 1024, v = 11.5, dir = math.pi*0.5, vrot = math.pi/64, mass = 48, size = 32,
+		texture = { {"gradient", {192, 128, 144, 255}, {128, 108, 128, 255}, 50}, {"blotch", {64, 64, 64, 64}, 4, 0.8}, {"blotch", {0, 0, 64, 64}, 3, 0.9} } })
+
+
 end
 
 function love.update(dt)
@@ -85,12 +85,28 @@ function love.keypressed(key, isrepest)
 end
 
 function love.draw()
+	local probe
+	local Tx, Ty = 0, 0
 	if #Space.probes >= 1 then
-		local probe = Space.probes[1]
+		probe = Space.probes[1]
 		love.graphics.push()
+		Tx, Ty = probe.x, probe.y
 		love.graphics.translate(love.window.getWidth()/2 - probe.x, love.window.getHeight()/2 - probe.y)
 	end
 	
+	local _x = math.floor(Tx / 1024) * 1024
+	local _y = math.floor(Ty / 1024) * 1024
+
+	love.graphics.draw(SpaceBG, _x-1024, _y-1024)
+	love.graphics.draw(SpaceBG, _x, _y-1024)
+	love.graphics.draw(SpaceBG, _x+1024, _y-1024)
+	love.graphics.draw(SpaceBG, _x-1024, _y)
+	love.graphics.draw(SpaceBG, _x, _y)
+	love.graphics.draw(SpaceBG, _x+1024, _y)
+	love.graphics.draw(SpaceBG, _x-1024, _y+1024)
+	love.graphics.draw(SpaceBG, _x, _y+1024)
+	love.graphics.draw(SpaceBG, _x+1024, _y+1024)
+
 	for i,B in ipairs(Physics.bodies) do
 		B:draw()
 	end
@@ -98,10 +114,20 @@ function love.draw()
 	
 	if #Space.probes >= 1 then
 		love.graphics.pop()
+
+		drawRadar(Radar, probe, 1/16)
+		drawNavWheel(NavWheel, probe)
+		love.graphics.draw(Radar, love.window.getWidth()-132, love.window.getHeight() - 132)
+		love.graphics.draw(NavWheel, 4, love.window.getHeight() - 132)
+
+		drawSegMeter(144, love.window.getHeight() - 66, 128, 16, {128, 0, 0, 255}, {255, 0, 0, 255}, 10, probe.boost_power, "up")
+
+		love.graphics.setFont(font.standard)
+		drawMeter(218, love.window.getHeight() - 10, 128, 16, {0, 64, 96, 255}, {0, 192, 255, 255}, probe.max_energy, probe.energy, "right")
+		love.graphics.printf("ENERGY:", 156, love.window.getHeight() - 28, 128, "left")
+		drawMeter(218, love.window.getHeight() - 42, 128, 16, {128, 64, 0, 255}, {255, 192, 0, 255}, probe.max_fuel, probe.fuel, "right")
+		love.graphics.printf("FUEL:", 156, love.window.getHeight() - 60, 128, "left")
+		drawMeter(218, love.window.getHeight() - 74, 128, 16, {64, 128, 0, 255}, {192, 255, 0, 255}, probe.max_booster, probe.booster, "right")
+		love.graphics.printf("BOOSTER:", 156, love.window.getHeight() - 92, 128, "left")
 	end
-	
-	drawRadar(Radar, Space.probes[1], 1/16)
-	drawNavWheel(NavWheel, Space.probes[1])
-	love.graphics.draw(Radar, love.window.getWidth()-132, love.window.getHeight() - 132)
-	love.graphics.draw(NavWheel, 4, love.window.getHeight() - 132)
 end
