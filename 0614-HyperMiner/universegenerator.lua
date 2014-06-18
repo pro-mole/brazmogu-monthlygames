370295da91db59a8df0bed46071e87dfb5e6c018
Professional Mole <professional.mole@yahoo.com.br>
2014-06-18 16:35:31 -0300

    Universe random generator added to the project

---
+++ 0614-HyperMiner/universegenerator.lua
@@ -0,0 +1,42 @@
+-- Universe Generator
+-- Here, shit gets real
+
+Universe = {
+	probes = {},
+	stars = {},
+	planets = {},
+	satellites = {},
+	meteors = {},
+	comets = {},
+	stations = {}
+}
+
+function Universe.generate()
+	-- Create star systems randomly
+end
+
+function Universe.createStar(x, y)
+	-- Generate star
+	
+	-- Add planetary sistems randomly
+	
+	-- Add comets
+	
+	-- Add meteors(roaming and belts)
+	
+	-- Add stellar space stations
+end
+
+function Universe.createPlanet(x, y, type)
+	-- Generate planet
+	
+	-- Add satellites
+	
+	-- Add planetary space stations
+end
+
+function Univesre.createSatellite(x, y)
+	-- Genearte satellite
+	
+	-- Add space station
+end
