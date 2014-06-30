36226c4482970e26854f9d6e1125f9145eb9f4e0
Professional Mole <professional.mole@yahoo.com.br>
2014-06-30 17:49:17 -0300

    Correcting the Universe table referenced in Satellite file Adding Asteroids and Comets(just baseic class implementation)

---
+++ 0614-HyperMiner/comet.lua
@@ -0,0 +1,52 @@
+-- Comet module
+--- A comet is a medium-sized celestial body composed of frozen liquid matter and some rock, that leaves a trail of material on its path
+
+require("physics")
+
+Comet = {
+	minerals = nil, -- Set of metallic concentration on the satellite's composition
+	mineral_depth = 1, -- Depth of metal deposits; raises as player drills
+	liquids = nil, -- Liquid composition on the planet's surface
+	liquid_depth = 1 -- Depth of liquid pools; raises as player pumps
+}
+
+Comet.__tostring = Body.__tostring
+
+function Comet:__index(index)
+	if rawget(self,index) ~= nil then
+		return self[index]
+	elseif Comet[index] ~= nil then
+		return Comet[index]
+	else
+		return Body[index]
+	end
+end
+
+function Comet.new(specs)
+	local T = Body.new(specs)
+	
+	local C = setmetatable(T, Comet)
+	C.class = 2
+
+	if C.minerals then
+		C.mineral_depth = 1
+	end
+	
+	table.insert(Universe.comets, C)
+	return C
+end
+
+function Comet:delete()
+	Body.delete(self)
+	
+	for i,S in ipairs(Universe.comets) do
+		if S == self then
+			table.remove(Universe.comets, i)
+			break
+		end
+	end
+end
+
+function Comet:draw()	
+	Body.draw(self)
+end
