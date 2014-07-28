389fbc9d7e1b2f252caddd1bf623699c693369a3
Professional Mole <professional.mole@yahoo.com.br>
2014-07-28 18:01:17 -0300

    Added August 2014 project to ignore list and checkout script

--- clone-branches.sh
+++ clone-branches.sh
@@ -4,11 +4,12 @@
 # Add new branches to the BRANCHES array and the corresponding folder name to the list of vars AND the .gitignore file
 
 REMOTE=https://github.com/pro-mole/brazmogu-monthlygames.git
-BRANCHES="0414 0514 0614 0714"
+BRANCHES="0414 0514 0614 0714 0814"
 BRANCH_0414=0414-PixelPopDefenseZone
 BRANCH_0514=0514-Molesweeper
 BRANCH_0614=0614-HyperMiner
 BRANCH_0714=0714-ColonizationChess
+BRANCH_0814=0814-Merchants
 
 for b in $BRANCHES
 do
